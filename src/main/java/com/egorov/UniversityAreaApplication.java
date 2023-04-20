c0ba3eb1c762b34414cbdae4265862997c306e6c
Egorov Ivan <iago.devs@yandex.ru>
2023-04-20 14:28:30 +0600

    Fixed: edit packages

--- src/main/java/com/egorov/UniversityAreaApplication.java
+++ src/main/java/com/egorov/UniversityAreaApplication.java
@@ -1,4 +1,4 @@
-package com.egorov.universityarea;
+package com.egorov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/egorov/UniversityAreaApplicationTests.java
+++ src/test/java/com/egorov/UniversityAreaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.egorov.universityarea;
+package com.egorov;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
