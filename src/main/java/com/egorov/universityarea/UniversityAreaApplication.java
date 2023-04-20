27d2cc310c04eb71959fc6c7590068d08b51a58e
Egorov Ivan <iago.devs@yandex.ru>
2023-04-20 13:29:27 +0600

    first commit

---
+++ src/main/java/com/egorov/universityarea/UniversityAreaApplication.java
@@ -0,0 +1,13 @@
+package com.egorov.universityarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityAreaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egorov/universityarea/UniversityAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egorov.universityarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityAreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
